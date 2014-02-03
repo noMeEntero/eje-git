498a6c9d626cfc741f3b7ae7dcd0fddec10db35d
usuario <usuario@10.0.2.15>
2014-02-03 17:54:12 +0100

    VERSION INICIAL

---
+++ Hola mundo/src/pruebaGit/pGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package pruebaGit;
+
+/**
+ * pablo sandoval usuario
+ * 
+ */
+public class pGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hola mundo");
+
+	}
+
+}
